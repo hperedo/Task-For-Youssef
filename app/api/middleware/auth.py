06b415b88e88e83ed64bf314c261357cb7f28ab5
Humberto Peredo Fuentes <hperedo1@yahoo.com.mx>
2025-04-19 18:55:00 -0600

    README.md requirements file uploaded

--- app/api/middleware/auth.py
+++ app/api/middleware/auth.py
@@ -2,9 +2,11 @@ from fastapi import Request, HTTPException
 from starlette.middleware.base import RequestResponseEndpoint  # Add this import
 from app.core.security import verify_token
 
-async def auth_middleware(request: Request, call_next: RequestResponseEndpoint):  # Add call_next
-    if request.url.path in ["/docs", "/openapi.json"]:
-        return await call_next(request)  # Pass request to call_next
+async def auth_middleware(request: Request, call_next):  # Add call_next
+    if request.url.path.startswith(("/redoc", "/docs")):
+        if request.headers.get("X-Docs-Auth") != "your-secret-key":
+            raise HTTPException(status_code=401)
+#        return await call_next(request)  # Pass request to call_next
     
     auth_header = request.headers.get("Authorization")
     if not auth_header:
